1c958f3903c0343bb5cd460e53064f8fc4089f05
dilanunver <dunver33@gmail.com>
2021-03-18 17:43:49 +0300

    useState-Array

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,36 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  const [message, setMessage] = useState('hello');
+  const [count, setCount] = useState(0);
+
+
+  const change = () => {
+    if (message === 'hello') {
+      setMessage('this is changed!');
+    } else {
+      setMessage('hello')
+    }
+  }
+  const increase = () => {
+    setCount(count + 1)
+  }
+  const decrease = () => {
+    setCount(count - 1)
+  }
+  const makeItZero = () => {
+    setCount(0)
+  }
+  return (
+    <div>
+      <h2>{message}</h2>
+      <h3>{count}</h3>
+      <button className="btn" onClick={() => decrease()}>Decrease</button>
+      <button className="btn" onClick={() => change()}>Change</button>
+      <button className="btn" onClick={() => increase()}>Increase</button>
+      <button className="btn" onClick={() => makeItZero()}>Make it Zero</button>
+    </div>
+  )
 };
 
 export default UseStateBasics;
